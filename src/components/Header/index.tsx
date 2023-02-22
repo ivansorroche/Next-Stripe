a941adb099e28d8c07c229da49a69dfdb91f1934
Ivan Burgos <ivan.sorroche@hotmail.com>
2023-02-22 19:55:22 -0300

    initial

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import styles from '../Header/styles.module.scss'
+import { SignInButton } from '../SignInButton'
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news"></img>
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+
+                <SignInButton />
+            </div>
+        </header>
+    )
+}
