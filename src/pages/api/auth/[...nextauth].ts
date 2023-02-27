0ae06ca29934116dbe4df17d2f3bc0832edea9ed
Ivan Burgos <ivan.sorroche@hotmail.com>
2023-02-27 19:30:12 -0300

    loggin with github

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,19 +1,55 @@
+import { fauna } from "../../../components/services/fauna"
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import GitHubProvider from "next-auth/providers/github";
+import { query as q } from 'faunadb'
 
 // interface GithubType {
 //   clientId: any,
 //   clientSecret: string
 // }
 
-export const authOptions = {
+export default NextAuth ({
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
+    GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     }),
   ],
-}
-export default NextAuth(authOptions)
+  callbacks : {
+    async signIn({user, account, profile}) {
+      const { email } = user
+      
+      try {
+       await fauna.query(
+        q.If(
+          q.Not(
+            q.Exists(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          ),
+          q.Create(
+             q.Collection('users'),
+             { data: { email: email }}
+            ),
+          q.Get(
+            q.Match(
+              q.Index('user_by_email'),
+              q.Casefold(user.email)
+                  )
+               )
+            )
+        )
+        return true;
+       }
+       
+      catch {
+        return false
+       }
+    }
+  }
+})
